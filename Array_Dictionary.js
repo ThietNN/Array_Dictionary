777c73a0b83204a3c19998765d8563b7516977e0
ThietNN <nguyenthiet1225@gmail.com>
2022-01-12 11:57:21 +0700

    edit

--- Array_Dictionary.js
+++ Array_Dictionary.js
@@ -1,13 +1,11 @@
+function translate(){
 let y = document.getElementById("input")
 let Vie = ["Xin chào","Tạm biệt","Hẹn gặp lại"];
-var x;
+let x;
 let Eng = ["Hello","Good bye", "See you again"];
 let result;
-
-
-function translate(){
     for(x=0;x<Vie.length;x++){
-        if (y == (Vie[x])){
+        if (y = (Vie[x])){
             result = Eng[x];
         }
         else{
