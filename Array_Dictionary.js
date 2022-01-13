9762d84d067071711b178109e04ec4e29b1b81b1
ThietNN <nguyenthiet1225@gmail.com>
2022-01-13 11:52:11 +0700

    edit

--- Array_Dictionary.js
+++ Array_Dictionary.js
@@ -1,16 +1,22 @@
-function translate(){
-let y = document.getElementById("input")
-let Vie = ["Xin chào","Tạm biệt","Hẹn gặp lại"];
-let x;
-let Eng = ["Hello","Good bye", "See you again"];
-let result;
+function translate() {
+    let y = document.getElementById("input");
+    let x;
+    let Vie = ["Xin chào", "Tạm biệt", "Hẹn gặp lại"];
+    let Eng = ["Hello", "Good bye", "See you again"];
+    let result = -1
     for(x=0;x<Vie.length;x++){
-        if (y = (Vie[x])){
-            result = Eng[x];
-        }
-        else{
-            result = "Không tìm thấy từ này trong từ điển"
+        if (y == (Vie[x])){
+            result = x;
+            break;
         }
     }
-    document.getElementById("result").innerText= result
+    if (result == -1){
+        alert("Từ bạn nhập không tồn tại trong từ điển");
+    }
+    else{
+        alert(Eng[x]);
+    }
+
 }
+
+
